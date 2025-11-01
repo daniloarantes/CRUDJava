3c8569b4b09dda773cf54b4040756ab9d3f26a18
Danilo Arantes <daniloarantes@gmail.com>
2025-10-31 22:29:43 -0300

    Criado  Base para adicionar usuário
    Criados objeto, model e DAO

--- src/main/java/BD/Script bd CrudProduto.sql
+++ src/main/java/BD/Script bd CrudProduto.sql
@@ -20,3 +20,26 @@ INSERT INTO tbl_produto(descricao, valor, quantidade)
 VALUES('Arroz', 2.50, 4);
 
 SELECT * FROM tbl_produto;
+
+
+CREATE TABLE tbl_usuarios (
+	id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nome varchar(80) NOT NULL,
+    login varchar(50) NOT NULL,
+    senha varchar(255) NOT NULL,
+    tipo varchar(10) NOT NULL
+    );
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
